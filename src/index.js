55473c53d4a10c4b360f6ca36dae5bff62e0bd80
Innocent Ingabire <inno100os@gmail.com>
2021-04-07 12:26:11 +0200

    some changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import fileupload from 'express-fileupload';
 import router from './routes/index.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 connectDB()
 // Security HTTP headers
