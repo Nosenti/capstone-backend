5d6823cbec76fe7709afdf82e61cbbc3cc37a570
Innocent Ingabire <inno100os@gmail.com>
2021-04-07 13:31:29 +0200

    changed cors policy

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import connectDB from './config/db.js';
 import 'dotenv/config.js';
 import fileupload from 'express-fileupload';
 import router from './routes/index.js';
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -41,6 +42,16 @@ const limiter = rateLimit({
 })
 
 app.use('/api', limiter)
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 
 app.use('/api', router);
